248915fa58894f9d49c0c4a2869b83e6cf7ff35e
Rick Scavetta <office@scavetta.academy>
2020-10-21 17:54:22 +0200

    add some notes and data

--- Mishal Updated ver.R
+++ Mishal Updated ver.R
@@ -2,12 +2,37 @@
 #Date:10/18/2020
 #Subject: Mishal - kaggle. 
 
-#downlaod Packages:
+# data source: https://www.kaggle.com/szamil/who-suicide-statistics
 
+# load packages
 library(tidyverse)
 
-#Read Data:
 
-us_disaster <- read.csv("who_suicide_statistics.csv")
+#Read Data:
+who <- read.csv("data/who_suicide_statistics.csv")
+
+## What are the kinds of questions we can answer with this dataset?
+
+# Suicide by country and year, has there been a change
+
+# Can we add more variables to this data that are not present?
+# What about GDP of each country?
+# What about historcial events? Did the suicide rate in eastern european countries rise after the fall of communism?
+# After chernobyl? what about in Japan after Fukushima?
+
+glimpse(who)
+library(gapminder)
+glimpse(gapminder)
+
+# First make sure each column is the same type. convert factor to character, or the other way around
+who <- who %>% 
+  mutate(country = as.factor(country))
+
+# now merge together..
+
+
+# is there any way to predict risk of suicide? i.e. other data...
+
+
 
 
